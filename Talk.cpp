8661d1a241b9d606c2c21cf98ce9125617f1cc03
javier.rivera8 <javier.rivera97@upr.edu>
2022-09-01 12:29:34 -0400

    while/exit

--- Talk.cpp
+++ Talk.cpp
@@ -4,17 +4,14 @@ using namespace std;
 
 int main(){
     string word;
-    int n = 0;
 
-    while(n < 5){
+    while(word != "exit"){
         cout<< "(TOM) - Listening... "<< endl;
         cout<< "(YOU) - ";
         cin>> word;
 
         cout<< "(TOM) - " + word <<endl;
-        n += 1;
-    }
-    
+    }  
 
     return 0;
 
