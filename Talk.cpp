0579a15725cc70810e7c04441ce8cf90a959753a
javier.rivera8 <javier.rivera97@upr.edu>
2022-09-01 12:04:07 -0400

    Input/Output Basics

---
+++ Talk.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string word;
+    cout<< "(TOM) - Listening... "<< endl;
+    cout<< "(YOU) - ";
+    cin>> word;
+
+    cout<< "(TOM) - " + word <<endl;
+
+    return 0;
+
+}
